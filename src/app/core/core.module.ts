245531141dd5223407f79b9cd604ce104027de71
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-07-01 00:11:35 +0200

    TP create project structure Store Films with Angular

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginModule } from '../login/login.module';
+import { IconsModule } from '../icons/icons.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { UiModule } from '../ui/ui.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [LoginModule, IconsModule, TemplatesModule, UiModule]
+})
+export class CoreModule { }
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+
+
+@NgModule({
+  declarations: [
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule
+  ]
+})
+export class LoginModule { }
