5f2fbdb2526c82d9b44afbae836d0fca0ec5463f
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-07-07 17:57:58 +0200

    add font awesome and icons nav

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconCloseComponent } from './icon-close/icon-close.component';
+import { IconNavComponent } from './icon-nav/icon-nav.component';
+import { IconEditComponent } from './icon-edit/icon-edit.component';
+import { IconDeleteComponent } from './icon-delete/icon-delete.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    IconCloseComponent,
+    IconNavComponent,
+    IconEditComponent,
+    IconDeleteComponent
+  ],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [
+    IconCloseComponent,
+    IconNavComponent,
+    IconEditComponent,
+    IconDeleteComponent
+  ],
 })
-export class IconsModule { }
+export class IconsModule {}
